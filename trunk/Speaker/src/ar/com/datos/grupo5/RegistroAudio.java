d6f0e3ab96298307ff0eb2743708758f7a53ac25
diegogarcia666@gmail.com <diegogarcia666@gmail.com@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-03-24 23:58:19 +0000

    Parece que los registros ya van a poder interactuar con los archivos

--- trunk/Speaker/src/ar/com/datos/grupo5/RegistroAudio.java
+++ trunk/Speaker/src/ar/com/datos/grupo5/RegistroAudio.java
@@ -2,26 +2,62 @@
 package ar.com.datos.grupo5;
 
 import ar.com.datos.grupo5.interfaces.Registro;
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
 
 /**
+ * Esta Clase implementa la interfaz del registro para audio.
+ * @see ar.com.datos.grupo5.interfaces.Registro
  * @author Diego
- *
  */
 public class RegistroAudio implements Registro {
 
 	private byte[] buffer;
-	private long offset;
-	private long longitud;
-	private AudioFileFormat.Type dato;
+	private Long longitud;
+	private byte[] dato;
 	
-	/* (non-Javadoc)
+	/* Método toBytes(). Nos sirve para devoler la cadena de bytes a escribir.
 	 * @see ar.com.datos.grupo5.interfaces.Registro#toBytes()
 	 */
 	public byte[] toBytes() {
-		// TODO Auto-generated method stub
-		return null;
+		String cadena;
+		/**
+		 * 1° Agrego la longitud.
+		 */
+		this.longitud = (long)this.dato.toString().length();
+		cadena = this.longitud.toString();
+		/**
+		 * Por ultimo, el dato.
+		 */
+		cadena += this.dato;
+		this.buffer = cadena.getBytes();
+		return this.buffer;
+	}
+
+	/**
+	 * @return the longitud
+	 */
+	public Long getLongitud() {
+		return longitud;
+	}
+
+	/**
+	 * @param longitud the longitud to set
+	 */
+	public void setLongitud(Long longitud) {
+		this.longitud = longitud;
+	}
+
+	/**
+	 * @return the dato
+	 */
+	public byte[] getDato() {
+		return dato;
+	}
+
+	/**
+	 * @param dato the dato to set
+	 */
+	public void setDato(byte[] dato) {
+		this.dato = dato;
 	}
 
 }
