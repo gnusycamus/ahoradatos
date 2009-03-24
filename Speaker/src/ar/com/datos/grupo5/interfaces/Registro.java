4c8e69b9e481003050189e8a2cbf74bc092d6dc0
diegogarcia666 <diegogarcia666@gmail.com>
2009-03-24 21:31:56 +0000

    Leves ajustes para el manejo de secuencial seguimos trabajando

--- Speaker/src/ar/com/datos/grupo5/interfaces/Registro.java
+++ Speaker/src/ar/com/datos/grupo5/interfaces/Registro.java
@@ -1,14 +1,12 @@
-/**
- * Esta clase representa a los registros que se graban en un archivo.
- */
+
 package ar.com.datos.grupo5.interfaces;
 
 /**
- * @author cristian
+ * Esta clase representa a los registros que se graban en un archivo.
+ * @author Cristian, Diego
  *
  */
 public interface Registro {
-
 	/**
 	 * Este metodo convierte el objeto en una tira de bytes.
 	 * @return la tira de bytes.
