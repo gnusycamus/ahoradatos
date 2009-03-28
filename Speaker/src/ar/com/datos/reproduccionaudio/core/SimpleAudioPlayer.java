2f057810ac379306257795c8bcbe8df55cd4a224
cristian5581 <cristian5581@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-03-28 15:08:07 +0000

    Agunos cambios menores de todo tipo, y las correcciones a la API de Audio de la catedra.

--- Speaker/src/ar/com/datos/reproduccionaudio/core/SimpleAudioPlayer.java
+++ Speaker/src/ar/com/datos/reproduccionaudio/core/SimpleAudioPlayer.java
@@ -14,77 +14,112 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import ar.com.datos.reproduccionaudio.exception.ForgotInitSimpleAudioPlayerException;
 import ar.com.datos.reproduccionaudio.exception.SimpleAudioPlayerException;
 
-public class SimpleAudioPlayer extends Thread{
+/**
+ * .
+ * @author cristian
+ *
+ */
+public class SimpleAudioPlayer extends Thread {
 	
-	private static final int	EXTERNAL_BUFFER_SIZE = 128000;
+	/**
+	 * 
+	 */
+	private static final int EXTERNAL_BUFFER_SIZE = 128000;
+	
+	/**
+	 * .
+	 */
 	private InputStream m_input;
-	private AudioInputStream m_audioInputStream; 
-	private SourceDataLine	m_line;
 	
-	public SimpleAudioPlayer(InputStream input){
+	/**
+	 * .
+	 */
+	private AudioInputStream m_audioInputStream;
+	
+	/**
+	 * .
+	 */
+	private SourceDataLine m_line;
+	
+	/**
+	 * 
+	 * @param input .
+	 */
+	public SimpleAudioPlayer(final InputStream input) {
 		m_input = input;
 	}
+	
+	/**
+	 * .
+	 * @throws SimpleAudioPlayerException .
+	 */
+	public final void init() throws SimpleAudioPlayerException {
 		
-	public void init()throws SimpleAudioPlayerException{
-		try{
+		try {
 			m_audioInputStream = AudioSystem.getAudioInputStream(m_input);
-			
-			AudioFormat	audioFormat = m_audioInputStream.getFormat();
-			
-			DataLine.Info	info = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+			AudioFormat audioFormat = m_audioInputStream.getFormat();
+
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class,
+					audioFormat);
 			m_line = (SourceDataLine) AudioSystem.getLine(info);
 			m_line.open(audioFormat);
-		}
-		catch (LineUnavailableException e)
-		{
+		} catch (LineUnavailableException e) {
 			throw new SimpleAudioPlayerException(e);
-		}
-		catch(IOException e){
+		} catch (IOException e) {
 			throw new SimpleAudioPlayerException(e);
-		}
-		catch(UnsupportedAudioFileException e){
+		} catch (UnsupportedAudioFileException e) {
 			throw new SimpleAudioPlayerException(e);
 		}
 	}
 
-	public void startPlaying()throws SimpleAudioPlayerException{
-		try{
+	/**
+	 * .
+	 * @throws SimpleAudioPlayerException .
+	 */
+	public final void startPlaying() throws SimpleAudioPlayerException {
+		try {
 			start();
-		}catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			throw new ForgotInitSimpleAudioPlayerException();
 		}
 	}
-	
-	public void start(){
+
+	/**
+	 * .
+	 */
+	public final void start() {
 		m_line.start();
 		super.start();
 	}
-	
-	public void stopPlaying(){
+
+	/**
+	 * .
+	 */
+	public final void stopPlaying() {
 		m_line.stop();
 		m_line.close();
 	}
-	
-	public void run(){
-		int	nBytesRead = 0;
-		byte[]	abData = new byte[EXTERNAL_BUFFER_SIZE];
-		while (nBytesRead != -1){
-			try
-			{
+
+	/**
+	 * .
+	 */
+	public final void run() {
+		int nBytesRead = 0;
+		byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
+		while (nBytesRead != -1) {
+			try {
 				nBytesRead = m_audioInputStream.read(abData, 0, abData.length);
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			if (nBytesRead >= 0)
-			{
+			if (nBytesRead >= 0) {
 				m_line.write(abData, 0, nBytesRead);
 			}
 		}
 		m_line.drain();
 		m_line.close();
-	}	
+	}
 
 }
 
