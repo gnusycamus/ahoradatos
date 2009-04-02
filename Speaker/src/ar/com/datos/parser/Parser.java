b34ee818845ed4ca5042a93710b2c84a59cd2820
gfuscax <gfuscax@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-04-02 17:06:29 +0000

    aao

--- Speaker/src/ar/com/datos/parser/Parser.java
+++ Speaker/src/ar/com/datos/parser/Parser.java
@@ -2,18 +2,15 @@ package ar.com.datos.parser;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import org.apache.log4j.Logger;
 
 import ar.com.datos.UnidadesDeExpresion.BufferedCollection;
 import ar.com.datos.UnidadesDeExpresion.IunidadDeHabla;
-import ar.com.datos.grupo5.Constantes;
 
 /**
  * @author zeke
@@ -29,7 +26,14 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 	private boolean moreLines;
 	private boolean esArchivo;
 	private String lineaSimple;
-
+   /**
+    * Constructor.
+    * Toma una cadena o archivo y realiza el procesado. Si es una linea,
+    * se utiliza un buffer para su tratamiento. 
+    * de palabras 
+    * @param rutaOlinea
+    * @param esArchivo
+    */
 	public Parser(final String rutaOlinea, boolean esArchivo) {
 
 		if (esArchivo) {
@@ -43,14 +47,13 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 						new FileNotFoundException(
 								"No existe el archivo pasado al Parser"));
 			}
-			try{
-			FileInputStream fis = new FileInputStream(archivo);
-			InputStreamReader isr = new InputStreamReader(fis, Constantes.DEFAULT_TEXT_INPUT_CHARSET);
-			buffer = new BufferedReader(isr);
+
+			try {
+				lector = new FileReader(archivo);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
-			catch(Exception e){}
-		
-		//	buffer = new BufferedReader(lector);
+			buffer = new BufferedReader(lector);
 
 		} else {
 			esArchivo = false;
@@ -59,39 +62,8 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 
 	}
 
-	
-	public Parser(final String rutaOlinea, boolean esArchivo, String charset) {
-
-		if (esArchivo) {
-			esArchivo = true;
-			this.ruta_archivo = rutaOlinea;
-			File archivo = new File(ruta_archivo);
-			milogueador = Logger.getLogger(Parser.class);
-
-			if (!archivo.exists()) {
-				milogueador.error("no existe el archivo",
-						new FileNotFoundException(
-								"No existe el archivo pasado al Parser"));
-			}
-			try{
-			FileInputStream fis = new FileInputStream(archivo);
-			InputStreamReader isr = new InputStreamReader(fis, charset);
-			buffer = new BufferedReader(isr);
-			}
-			catch(Exception e){}
-		
-		//	buffer = new BufferedReader(lector);
-
-		} else {
-			esArchivo = false;
-			lineaSimple = rutaOlinea;
-		}
-
-	}
-	
-	
 	/**
-	 * @param caca
+	 * Metodo que devuelve la proxima linea a procesar
 	 * @throws IOException
 	 */
 	private String leerLinea() {
@@ -99,7 +71,7 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 		String linea = "";
 
 		while ((linea != null) && (linea.isEmpty())) {
-			try {
+			try { /*Carga una nueva linea en del buffer*/
 				linea = (buffer.readLine());
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -115,7 +87,9 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 			return null;
 		}
 	}
-
+   /**
+    *  Metodo que 
+    */
 	public final boolean hasMoreItems() {
 		return moreLines;
 	}
@@ -145,7 +119,12 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 			materiaPrima = this.leerLinea();
 		}
 	}
-
+    /**
+     * Metodo que procesa una la linea, separandola por palabra y aplicando un filtrado
+     * a caracteres invalidos y agregando las palabras obtenidas a la coleccion pasada 
+     * por parametro
+     * @param coleccion
+     */
 	private void procesarLineaSimple(Collection<IunidadDeHabla> coleccion) {
 
 		String[] listaPalabras;
@@ -160,7 +139,10 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 			}
 		}
 	}
-
+    /**
+     *  Metodo que devuelve una coleccion para ser usada
+     *  en bufferización
+     */
 	public Collection<IunidadDeHabla> listar() {
 
 		BufferedCollection<IunidadDeHabla> coleccionBuf = new BufferedCollection<IunidadDeHabla>(
@@ -168,7 +150,10 @@ public class Parser implements BufferRecharger<IunidadDeHabla> {
 		recargarBuffer(coleccionBuf, 1);
 		return coleccionBuf;
 	}
-
+    /**
+     * Método que genera una colection para el tratamiento de palabras
+     * @return Collection 
+     */
 	public Collection<IunidadDeHabla> simpleString() {
 
 		ArrayList<IunidadDeHabla> coleccion = new ArrayList<IunidadDeHabla>();
