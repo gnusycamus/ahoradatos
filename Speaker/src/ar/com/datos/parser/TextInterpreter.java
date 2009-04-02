687d57f97033e490d8a8c749010e254bfe680fe1
ramiro.salom <ramiro.salom@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-04-02 22:42:18 +0000

    DesHarcodee el false que recibe en esArchivo

--- Speaker/src/ar/com/datos/parser/TextInterpreter.java
+++ Speaker/src/ar/com/datos/parser/TextInterpreter.java
@@ -20,7 +20,7 @@ public class TextInterpreter implements ITextInput {
 	public Collection<IunidadDeHabla> modoLectura(String rutaOlinea,
 			boolean esArchivo) throws Exception {
 
-		Parser miparser = new Parser(rutaOlinea, false);
+		Parser miparser = new Parser(rutaOlinea, esArchivo);
 		if (esArchivo) {
 			return miparser.listar();
 		} else {
