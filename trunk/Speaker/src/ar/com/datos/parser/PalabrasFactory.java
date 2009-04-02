cbcca671f17e912625546d1f4520a75a53cac74c
gfuscax <gfuscax@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-04-02 17:06:29 +0000

    aao

--- trunk/Speaker/src/ar/com/datos/parser/PalabrasFactory.java
+++ trunk/Speaker/src/ar/com/datos/parser/PalabrasFactory.java
@@ -3,6 +3,15 @@ package ar.com.datos.parser;
 import ar.com.datos.UnidadesDeExpresion.Palabra;
 import ar.com.datos.grupo5.Constantes;
 
+/**
+ * Factory de palabras, que si esta activado la optimización al español,
+ * se genera un proceso de limpiado de la palabra por su fonética de modo que,
+ * solo se tienen en cuenta letras de la palabra que influyan a su pronunciación,
+ * evitando palabras foneticamente repetidas
+ * @see PatterRecognizer
+ * @author gabriel
+ *
+ */
 public class PalabrasFactory {
 
 	public static Palabra getPalabra(String palabraEscrita) {
