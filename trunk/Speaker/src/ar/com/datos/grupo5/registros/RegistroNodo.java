3882088ad814556e22b57563b250a0eb654ffb59
diegogarcia666 <diegogarcia666@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-04-25 18:50:13 +0000

    Clase RegistroNodo. Es la estructura para registros de un nodo genérico

---
+++ trunk/Speaker/src/ar/com/datos/grupo5/registros/RegistroNodo.java
@@ -0,0 +1,93 @@
+
+package ar.com.datos.grupo5.registros;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+
+import ar.com.datos.grupo5.Constantes;
+import ar.com.datos.grupo5.ListasInvertidas;
+import ar.com.datos.grupo5.ParFrecuenciaDocumento;
+import ar.com.datos.grupo5.btree.Clave;
+import ar.com.datos.grupo5.interfaces.Registro;
+import ar.com.datos.grupo5.utils.Conversiones;
+
+/**
+ * Esta clase implementa el registro para los nodos de árboles.
+ * 
+ * @see ar.com.datos.grupo5.interfaces.Registro
+ * @author LedZeppeling
+ */
+public class RegistroNodo implements Registro {
+	
+	/**
+	 * Logger.
+	 */
+	private static Logger logger  = Logger.getLogger(ListasInvertidas.class);
+
+	/**
+	 * Cuantos bytes puedo pasar.
+	 */
+	private Long moreBytes;
+	
+	/**
+	 * Es la clave del nodo.
+	 */
+	private Clave claveNodo;
+	
+	/**
+	 * En este caso se devuelve de una vez todos los bytes. Devuelvo true la
+	 * primera vez y pongo en false, despues cuando se pregunta nuevamente
+	 * devuelvo false, pero pongo en true para que el registro pueda ser usado
+	 * denuevo.
+	 * 
+	 * @return true si hay mas bytes para pedir con getBytes.
+	 */
+	public final boolean hasMoreBytes() {
+		
+		if (moreBytes > 0) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @see ar.com.datos.grupo5.interfaces.Registro#toBytes()
+	 * @return los bytes que representan al registro.
+	 */
+	public final byte[] getBytes() {
+		//TODO Hacer este método!!!!!!
+		byte[] arraybytes = null;
+		return arraybytes;
+	}
+	
+	/**
+	 * Método que llena los atributos a partir de lo contenido en el buffer.
+	 * @param buffer Cadena de Bytes leida en el archivo de bloques
+	 * @param offset id del termino que se busca.
+	 */
+	public final void setBytes(final byte[] buffer, final Long offset) {
+	// TODO Hacer este método!!!!	
+	}
+
+	/**
+	 * En el caso de ocupar varios bloques esta función agrega mas datos
+	 * al registro a partir de los datos de otros bloques.
+	 * @param buffer
+	 * 			Datos leidos de un bloque que no es el primero.
+	 * @param offset
+	 * 			Offset dentro de les array de byte donde empiezan
+	 * 			los datos.
+	 */
+	public final void setMoreBytes(final byte[] buffer, final int offset) {
+		//TODO Hacer este método!!!!!!!!!		
+	}
+	
+}
