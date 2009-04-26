24d4ea80c1d972b1d551aa4d038aacd2d51a2349
ramiro.salom <ramiro.salom@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-04-26 16:56:06 +0000

    main con el que estoy probando el archivo de bloques.

---
+++ Speaker/src/ar/com/datos/grupo5/TestRam.java
@@ -0,0 +1,43 @@
+package ar.com.datos.grupo5;
+
+import java.io.FileNotFoundException;
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * @author xxvkue
+ *
+ */
+public final class TestRam {
+
+	/**
+	 * .
+	 */
+	private static Logger logger = Logger.getLogger(TestRam.class);
+	/**
+	 * .
+	 */
+	private TestRam() {
+		
+	}
+	/**
+	 * @param args
+	 * 	Argumento
+	 */
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		ListasInvertidas listasI = new ListasInvertidas();
+		
+		/*
+		 * Abro el archivo para la carga y consulta del diccionario
+		 */
+		try {
+			listasI.abrir(Constantes.ARCHIVO_LISTAS_INVERTIDAS,
+					Constantes.ABRIR_PARA_LECTURA_ESCRITURA);
+		} catch (FileNotFoundException e) {
+			logger.debug("No se pudo abrir el diccionario.");
+		}
+
+		logger.debug("Abrio el archivo Diccionario");
+	}
+}
