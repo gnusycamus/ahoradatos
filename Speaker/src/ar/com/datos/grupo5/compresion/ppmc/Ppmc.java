8f330e42d740ddd8f4a4cd19a594184b742698cc
ramiro.salom <ramiro.salom@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-06-01 19:28:46 +0000

    Agrego clases de Ppmc

---
+++ Speaker/src/ar/com/datos/grupo5/compresion/ppmc/Ppmc.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author xxvkue
+ *
+ */
+public class Ppmc {
+
+	private ArrayList<ParCharProb> contextoOrdenMenosUno;
+	
+	public Ppmc(){
+		//TODO: Llenar contextoOrdenMenosUno con los caracteres validos del UNICODE
+	}
+	
+	private final ArrayList<ParCharProb> 
+		obtenerExclusionCompleta(final ArrayList<parCharProb> listaActual, final ArrayList<ParCharProb> listaContextoAnterior){
+		//TODO:Implementar un maldito bucle para obtener la exclusion.
+		return new ArrayList<ParCharProb>();
+	}
+}
