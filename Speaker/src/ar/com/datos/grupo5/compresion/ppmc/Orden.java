2d1862b11be8ac1a625217d46d6808fce002d04e
ramiro.salom <ramiro.salom@3d2696ce-0fcf-11de-8ca9-75d9e183a346>
2009-06-02 05:05:10 +0000

    Avances pero con muchas dudas!!, Miles de FIXME!!! MUEJEJEJEJEJE, si diabolico lo sé.

--- Speaker/src/ar/com/datos/grupo5/compresion/ppmc/Orden.java
+++ Speaker/src/ar/com/datos/grupo5/compresion/ppmc/Orden.java
@@ -1,33 +1,82 @@
+package ar.com.datos.grupo5.compresion.ppmc;
 /**
  * 
  */
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import ar.com.datos.grupo5.compresion.aritmetico.ParCharProb;
 /**
- * @author xxvkue
+ * @author Led Zeppiln
  *
  */
 public class Orden {
 
 	private int Numero;
-	private HashMap<String,ParCharProb> listaContexto;
+	private HashMap<String, HashMap<Character,ParCharProb> > listaContexto;
 	
 	public Orden(){
 		
 	}
 	
-	public final HashMap<String,ParCharProb> obtenerListaContextos(){
-		return this.listaContexto;
+	/**
+	 * Obtengo la lista de ocurrencias en el contexto recibido.
+	 * @param contexto Contexto del cual voy a buscar la lista de ocurrencias. 
+	 * @return Lista de ocurrencias de las letras.
+	 */
+	public final HashMap<Character,ParCharProb> obtenerListaContextos(String contexto){
+		return this.listaContexto.get(contexto);
 	}
 	
-	public final void incrementarFrecuenciaChar(Character letra){
+	/**
+	 * Incrementa la frecuencia del char que se emitio, sino existia el contexto se 
+	 * crea.
+	 * @param contexto Contexto en el cual actulizo la frecuencia.
+	 * @param letra Letra de la cual actualizo la frecuencia.
+	 */
+	public final void incrementarFrecuenciaChar(String contexto, Character letra){
 		ParCharProb par;
-		if (this.listaContexto.containsKet()){
-			par = this.listaContexto.get(letra);
-			par.setProbabilidad(par.getProbabilidad() + 1);
+		if (this.listaContexto.containsKey(contexto)) {
+			//El contexto Existe
+			if (this.listaContexto.get(contexto).containsKey(letra)){
+				par = this.listaContexto.get(contexto).get(letra);
+				par.setProbabilidad(par.getProbabilidad() + 1);
+			} else {
+				par = new ParCharProb(letra, 1);
+				this.listaContexto.get(contexto).put(letra, par);
+			}
 		} else {
-			par.setSimboloUnicode(letra);
-			par.setProbabilidad(1);
+			//Creo el contexto
+			this.crearContexto(contexto, letra);
 		}
 		
+	}
+	
+	/**
+	 * Creo el contexto, lo inicializo con el caracter buscado y el escape en 1  
+	 * @param contexto Contexto a crear.
+	 * @param letra Letra a agregar en ese contexto.
+	 */
+	private final void crearContexto(String contexto, Character letra) {
+		
+		ParCharProb par;
+		//El contexto no existe por lo tanto lo creo
+		this.listaContexto.put(contexto, new HashMap<Character,ParCharProb>());
+		
+		//FIXME: Para el EOF QUE PONGO!!!, me conviene cambiarlo a String
+		par = new ParCharProb(letra, 1);
+		this.listaContexto.get(contexto).put(letra, par);
+		
+		//Creo el EOF, ver que pasa aca.
+		par = new ParCharProb(new Character(' '), 1);
+	
+	}
+	
+	/**
+	 * Obtiene todos los valores del HashMap
+	 * @return Un vector de Objects.
+	 */
+	public final Object[] obtenerArrayList(String contexto){
+		return this.listaContexto.get(contexto).entrySet().toArray();
 	}
 }
--- Speaker/src/ar/com/datos/grupo5/compresion/ppmc/Ppmc.java
+++ Speaker/src/ar/com/datos/grupo5/compresion/ppmc/Ppmc.java
@@ -1,22 +1,155 @@
+package ar.com.datos.grupo5.compresion.ppmc;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import ar.com.datos.grupo5.Constantes;
+import ar.com.datos.grupo5.compresion.aritmetico.ParCharProb;
+
 /**
  * 
  */
 
 /**
- * @author xxvkue
+ * @author Led Zeppelin
  *
  */
 public class Ppmc {
 
 	private ArrayList<ParCharProb> contextoOrdenMenosUno;
 	
+	private ArrayList<Orden> listaOrdenes;
+	
+	private String contextoActual;
+	
+	private int orden;
+	
+	/**
+	 * Contructor de clase.
+	 */
 	public Ppmc(){
 		//TODO: Llenar contextoOrdenMenosUno con los caracteres validos del UNICODE
+		
+		//Obtengo el orden del xml de configuración
+		this.orden = Constantes.ORDER_MAX_PPMC;
+		//Creo tantos Ordenes como dice el XML
+		this.listaOrdenes = new ArrayList<Orden>(orden+1);
 	}
 	
+	/**
+	 * Genera un lista nueva con las letras posibles en el contexto.
+	 * @param listaActual	Lista a la que voy a filtrar por el contexto de orden mayor.
+	 * @param listaContextoAnterior Lista del contexto del orden mayor.
+	 * @return Lista con la exclusion aplicada.
+	 */
 	private final ArrayList<ParCharProb> 
-		obtenerExclusionCompleta(final ArrayList<parCharProb> listaActual, final ArrayList<ParCharProb> listaContextoAnterior){
-		//TODO:Implementar un maldito bucle para obtener la exclusion.
-		return new ArrayList<ParCharProb>();
+		obtenerExclusionCompleta(final ArrayList<ParCharProb> listaActual, final ArrayList<ParCharProb> listaContextoAnterior){
+		ArrayList<ParCharProb> nuevaListaContexto = new ArrayList<ParCharProb>();
+		
+		nuevaListaContexto.addAll(listaActual);
+		if (listaContextoAnterior != null) {
+			//FIXME: Ojo al piojo, no eliminar el escape!!, Son iguales solo por el char o por el char y la prob??
+			nuevaListaContexto.removeAll(listaContextoAnterior);
+		}
+		
+		return nuevaListaContexto;
 	}
+	
+	/**
+	 * Obtiene el contexto de la posición actual
+	 * @param cadena Cadena a comprimir
+	 * @param posicion Posición dentro de la cadena a comprimir
+	 * @return El contexto en la posición posicion
+	 */
+	private final void obtenerContexto(final String cadena, final int posicion){
+		//FIXME: Ver si es > o >=
+		if (posicion > this.orden) {
+			//Obtengo un contexto de maximo orden
+			// casados -> pos = d => el contexto = casa
+			// casados -> pos = o => el contexto = asad
+			contextoActual = cadena.substring(posicion - this.orden, posicion);
+		} else {
+			//Obtengo un contexto parcial por no tener un contexto mayor
+			// casados -> pos = s => el contexto = ca
+			contextoActual = cadena.substring(0, posicion);
+		}
+	}
+	
+	/**
+	 * Comprime la cadena recibida bajo el metodo PPMC
+	 * @param cadena Cadena a comprimir.
+	 * @return Un Array de bytes para escribir a archivo.
+	 */
+	public final byte[] comprimir(final String cadena){
+		int pos = 0;
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();  
+		DataOutputStream dos = new DataOutputStream(bos);
+		
+		while (pos < cadena.length()) {
+			this.obtenerContexto(cadena, pos);
+			try {
+				dos.write(this.recorrerContextos(cadena.charAt(pos)));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+		}
+		
+		return bos.toByteArray();
+	}
+
+	private final byte[] recorrerContextos(Character letra) {
+		int ordenContexto = this.contextoActual.length();
+		String contexto = this.contextoActual.substring(0, ordenContexto); 
+		boolean finalizarRecorrida = false;
+		//Recorro los Contextos desde el orden indicado por el largo del contexto
+		ArrayList<ParCharProb> listaContextoMasUno;
+		ArrayList<Character> NroRes;
+		while (ordenContexto > -1 && !finalizarRecorrida){
+			ArrayList<ParCharProb> listaContextoActual = new ArrayList<ParCharProb>();
+			//Recorro los contextos
+			
+			//FIXME: no se como obtener la lista de values que quiero.
+			//listaContextoActual.addAll(this.listaOrdenes.get(ordenContexto).obtenerArrayList(contexto));
+			
+			//FIXME: no se como obtener la lista de values que quiero.
+			if (ordenContexto < this.orden) {
+				
+				listaContextoMasUno = new ArrayList<ParCharProb>();
+			//listaContextoMasUno.add(this.listaOrdenes.get(ordenContexto+1).obtenerArrayList(contexto));
+				
+			} else {
+				listaContextoMasUno = null;
+			}
+			
+			ArrayList<ParCharProb> nuevoOrdenContexto = this.obtenerExclusionCompleta(listaContextoActual, listaContextoMasUno);
+			
+			//FIXME: LLamar al Compresor Aritmetico, si no hay nada como lo hago?? le mando un ESC o 
+			//solo le mando null en el nuevoOrdenContexto
+			if (nuevoOrdenContexto == null || nuevoOrdenContexto.size() == 0) {
+				//NroRes = this.comprimir(null,letra);
+			} else {
+			//NroRes = this.comprimir(nuevoOrdenContexto,letra);
+			}
+		
+			//Actualizo la frecuencia en el contexto particular
+			this.listaOrdenes.get(ordenContexto).incrementarFrecuenciaChar(contexto, letra);
+		
+			//Actualizo el proximo contexto
+			contexto = this.contextoActual.substring(this.orden - ordenContexto, ordenContexto);
+		}
+		if (ordenContexto == -1 && !finalizarRecorrida){
+			//Analizo por separado el ultimo vector 
+		}
+		
+		//FIXME: Convertir NroRes a bytes y devolverlo
+		return new byte[2];
+	}
+	
 }
